84a308444439e681f904cf34c24b579c7e995e9c
김석진 <7271kim@naver.com>
2020-10-02 03:31:02 +0900

    [2020.10.02] 표준편차 추가

--- src/main/java/com/seokjin/kim/library/MathAll.java
+++ src/main/java/com/seokjin/kim/library/MathAll.java
@@ -17,11 +17,49 @@ public class MathAll {
         return result;
     }
     
+    /**
+     * 조합 케이스 nCr 획득
+     */
     public static Set<String> getCombination( String[] n, int r ){
         Set<String> result = new LinkedHashSet<>();
         checkArrayCombination( n, r, 0, result, "", 0);
         return result;
     }
+    
+    /**
+     * 표준편차 구하기
+     * 표준편차 = 분산의 제곱근
+     * 분산 = 편차 제곱의 합 / 총 변량의 개수
+     */
+    public static double standardDeviation( double[] array ){
+        
+        if (array.length < 2) return Double.NaN;
+        
+        double sum = 0.0;
+        double sd = 0.0;
+        double diff;
+        double meanValue = mean(array);
+
+        for (int i = 0; i < array.length; i++) {
+          diff = array[i] - meanValue;
+          sum += diff * diff;
+        }
+        sd = Math.sqrt(sum /array.length);
+
+        return sd;
+    }
+    
+    /**
+     * 배열의 평균 구하기
+     */
+    public static double mean(double[] array) {  // 산술 평균 구하기
+        double sum = 0.0;
+
+        for (int i = 0; i < array.length; i++)
+          sum += array[i];
+
+        return sum / array.length;
+      }
     
     /*
      * 종료 조건 : r개 뽑앗을 때,
